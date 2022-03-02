c46c43d6f09a53405cbffbb6c24a7c4b7397e8d9
dezhiShen <1179551960@qq.com>
2022-03-02 22:34:55 +0800

    mod:修改下载路径

--- all-in-one.sh
+++ all-in-one.sh
@@ -347,7 +347,10 @@ if [ "$flag" = "y" ];then
             -v /docker_data/public/:/public \
         p3terx/aria2-pro
 
-        sleep 10
+        while [ ! -f $base_data_dir/aria2/aria2.conf ]; do
+            echo "等待aria2启动"
+            sleep 1
+        done
         
         echo "修改aria2默认下载路径"
         sed -i `echo "s/dir=\\/downloads/dir=\\/public\\/downloads/g"` $base_data_dir/aria2/aria2.conf
