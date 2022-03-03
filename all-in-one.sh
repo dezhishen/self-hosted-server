205a5863b9470a2b55baa008220cd3309bb406cb
dezhishen <1179551960@qq.com>
2022-03-03 13:45:13 +0800

    feat:修改和增加中文打印

--- all-in-one.sh
+++ all-in-one.sh
@@ -17,10 +17,12 @@ while getopts p:d:sugh OPTION; do
         generatessl=y
         ;;
     h)
-        echo "-d set domian -u enable auto update ssl cert -p set base data dir -s enable ssl -g generate ssl cert"
+        echo "-d set domian -p set base data dir -s enable ssl -g generate ssl cert -u enable auto update ssl cert"
+        echo "-d set 根域名 -p 设置存储根目录 -s 启用https -g 生成https证书 -u 自动更新https证书"
         exit 1;;
     ?)
         echo "get a non option $OPTARG and OPTION is $OPTION"
+        echo "非法参数 $OPTARG:$OPTION"
         exit 1;;
     esac
 done
@@ -28,16 +30,18 @@ done
 if [ ! -n "$domain" ]; then  
     domain=$(./scripts/read-args-with-history.sh domain) 
     if [ ! -n "$domain" ]; then  
-        read -p "please input the domain of your home server:" domain
+        echo "请输入根域名"
+        read -p "please input root domain:" domain
         if [ ! -n "$domain" ]; then  
             domain="self.docker.com"
         fi
-        ./scripts/set-args-to-history.sh domain $domain
+        sh ./scripts/set-args-to-history.sh domain $domain
     fi
 fi
 if [ ! -n "$base_data_dir" ]; then  
     base_data_dir=$(./scripts/read-args-with-history.sh base_data_dir)
     if [ ! -n "$base_data_dir" ];then
+        echo "请输入docker卷使用的根目录,默认为/docker_data"
         read -p "please input the base data dir of your home server,default is /data/data:" base_data_dir
         if [ ! -n "$base_data_dir" ]; then  
             base_data_dir="/docker_data"
@@ -49,6 +53,7 @@ fi
 if [ ! -n "$ssl" ]; then
     ssl=$(./scripts/read-args-with-history.sh ssl)
     if [ ! -n "$ssl" ]; then
+        echo "是否启用https?[y/n]"
         read -p "Do you want to enable ssl? [y/n]: " yn
         case $yn in
             [Yy]* )
@@ -68,6 +73,7 @@ fi
 if [ ! -n "$generatessl" ]; then  
     generatessl=$(./scripts/read-args-with-history.sh generatessl)
     if [ ! -n "$generatessl" ]; then
+        echo "是否生成https证书?[y/n]"
         read -p "Do you want to generate ssl cert? [y/n]: " yn
         case $yn in
             [Yy]* )
@@ -87,6 +93,7 @@ fi
 if [ ! -n "$autossl" ]; then  
     autossl=$(./scripts/read-args-with-history.sh autossl)
     if [ ! -n "$autossl" ]; then
+        echo "是否自动更新https证书?[y/n]"
         read -p "Do you want to enable auto update ssl cert? [y/n]: " yn
         case $yn in
             [Yy]* )
@@ -102,13 +109,20 @@ if [ ! -n "$autossl" ]; then
         ./scripts/set-args-to-history.sh autossl $autossl
     fi
 fi
-
+echo "你的服务配置如下"
 echo "the config of your home server is:"
+printf "根域名: %s\n" $domain
 printf "domain: %s\n" $domain
+printf "存储根目录: %s\n" $base_data_dir
 printf "base_data_dir: %s\n" $base_data_dir
+printf "是否启用https: %s\n" $ssl
 printf "ssl: %s\n" $ssl
+printf "是否生成https证书: %s\n" $generatessl
 printf "autossl: %s\n" $autossl
+printf "是否自动更新https证书: %s\n" $autossl
 printf "generatessl: %s\n" $generatessl
+
+echo "请确认是否正确，输入y确认，输入n退出"
 read -p "Are you sure? [y/n]: " yn
 case $yn in
     [Yy]* )
@@ -126,20 +140,21 @@ export autossl=$autossl
 export generatessl=$generatessl
 
 # create base data dir if not exist
+echo "创建存储根目录"
 echo "create base data dir if not exist"
 if [ ! -d $base_data_dir ]; then
     mkdir -p $base_data_dir
+    echo "创建存储根目录成功"
     echo "create base data dir $base_data_dir success"
 else
     # do you want to backup old data dir?
+    echo "是否备份旧的存储目录"
     read -p "Do you want to backup old data dir? [y/n]: " yn
     case $yn in
         [Yy]* )
-            read -p "please input the backup dir:" backup_dir
-            if [ ! -d $backup_dir ]; then
-                backup_dir=$base_data_dir.bak.$(date +%Y%m%d%H%M%S)
-            fi
+            backup_dir=$base_data_dir.bak.$(date +%Y%m%d%H%M%S)
             copy -f $base_data_dir $backup_dir
+            echo "备份存储目录[$backup_dir]成功"
             echo "backup old data dir $base_data_dir to $backup_dir success"
             ;;
         [Nn]* )
@@ -149,11 +164,23 @@ else
     esac
 fi
 
+# generate ssl cert
+case $generatessl in
+    [Yy]* )
+        ./scripts/generate-ssl-cert.sh
+        ;;
+    [Nn]* )
+        ;;
+    * )
+        ;;
+esac
+
 # create docker network
 ## print docker network list
 ## input or choose your docker network name,default is ingrees
 docker_network_name=$(./scripts/read-args-with-history.sh docker_network_name)
 if [ ! -n "$docker_network_name" ]; then
+    echo "请输入docker网络名称,默认为ingrees"
     read -p "please input your docker network name,default is ingrees:" docker_network_name
     if [ ! -n "$docker_network_name" ]; then
         docker_network_name="ingrees"
@@ -162,80 +189,55 @@ if [ ! -n "$docker_network_name" ]; then
 fi
 export docker_network_name=$docker_network_name
 
-./scripts/create-docker-network.sh $docker_network_name
+sh ./scripts/create-docker-network.sh $docker_network_name
 
 # insatll/reinstall portainer
+echo "是否安装/重装portainer"
 read -p "do you want to install/reinstall portainer ? [y/n]:" yn
 case $yn in
     [Yy]* )
-        echo "installing portainer"
+        #echo "installing portainer"
         eval ./scripts/install-portainer.sh
-        ;;
-    [Nn]* )
-        echo "skip portainer"
-        ;;
-    * )
-        echo "skip portainer"
         ;;
 esac
 # install/reinstall filebrowser
+echo "是否安装/重装filebrowser"
 read -p "do you want to install/reinstall filebrowser ? [y/n]:" yn
-
 case $yn in
     [Yy]* )
-        echo "installing filebrowser"
+        #echo "installing filebrowser"
         ./scripts/install-filebrowser.sh
-        ;;
-    [Nn]* )
-        echo "skip filebrowser"
-        ;;
-    * )
-        echo "skip filebrowser"
         ;;
 esac
 # install/reinstall adguardhome
+echo "是否安装/重装adguardhome"
 read -p "do you want to install/reinstall adguardhome ? [y/n]:" yn
 case $yn in
     [Yy]* )
-        echo "installing adguardhome"
+        #echo "installing adguardhome"
         ./scripts/install-adguardhome.sh
-        ;;
-    [Nn]* )
-        echo "skip adguardhome"
-        ;;
-    * )
-        echo "skip adguardhome"
         ;;
 esac
 
 # install/reinstall webssh2 with warning "webssh2 is not support on arm"
+echo "[警告] webssh2 在arm平台上无法运行"
+echo "是否安装/重装webssh2"
 echo "[warning] webssh2 is not support on arm"
 read -p "do you want to install/reinstall webssh2 ? [y/n]:" yn
 case $yn in
     [Yy]* )
-        echo "installing webssh2"
+        #echo "installing webssh2"
         ./scripts/install-webssh2.sh
-        ;;
-    [Nn]* )
-        echo "skip webssh2"
-        ;;
-    * )
-        echo "skip webssh2"
         ;;
 esac
 
 # install/reinstall navidrome
+echo "是否安装/重装navidrome"
 read -p "do you want to install/reinstall navidrome ? [y/n]:" yn
 case $yn in
     [Yy]* )
-        echo "installing navidrome"
+        #echo "installing navidrome"
     ./scripts/install-navidrome.sh
-        ;;
-    [Nn]* )
-        echo "skip webssh2"
-        ;;
-    * )
-        echo "skip webssh2"
         ;;
 esac
 
@@ -243,6 +245,7 @@ esac
 # install/reinstall vaultwarden
 case $ssl in
 [yY]*)
+    echo "是否安装/重装vaultwarden"
     echo "do you want to install/reinstall vaultwarden ? [y/n]:"
     read flag
     if [ "$flag" = "y" ];then
@@ -250,43 +253,32 @@ case $ssl in
     fi
     ;;
 *)
-echo "vaultwarden must running with ssl , skip vaultwarden"
-;;
+    echo "vaultwarden 必须使用https 才能安装"
+    echo "vaultwarden must running with ssl , skip vaultwarden"
+    ;;
 esac
 
 # install/reinstall aria2
+echo "是否安装/重装aria2"
 read -p "do you want to install/reinstall aria2 ? [y/n]:" yn
 
 case $yn in
     [Yy]* )
-        echo "installing aria2"
+        #echo "installing aria2"
         ./scripts/install-aria2.sh
-        ;;
-    [Nn]* )
-        echo "skip aria2"
-        ;;
-    * )
-        echo "skip aria2"
         ;;
 esac
 
 # install/reinstall nginx
+echo "是否安装/重装nginx"
 read -p "do you want to install/reinstall nginx ? [y/n]:" yn
 case $yn in
     [Yy]* )
-        echo "installing nginx"
         ./scripts/install-nginx.sh
         ;;
-    [Nn]* )
-        echo "skip nginx"
-        ;;
-    * )
-        echo "skip nginx"
-        ;;
 esac
-
-echo "You have finished the install and your home server is ready after all docker container ready"
-
-echo "There is docker container status or you can run docker ps -a to check them,thinks for used this script"
-
+echo "感谢使用本安装脚本，你已经完成了所有的安装，当docker容器准备好后，你的服务就可以被访问了"
+echo "thanks for used this script,you have finished all install,when docker container ready,your service can be accessed"
+echo "即将退出和打印当前docker容器运行的状态"
+echo "will exit and print current docker container status"
 docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
